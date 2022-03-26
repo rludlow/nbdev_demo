263df6bd91ee9445854fc12f55d3c72025afd08b
richardludlow <richard.ludlow@gmail.com>
2022-03-26 14:33:35 -0700

    check in files

---
+++ 00_core.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# default_exp core
+
+
+# # jaguar
+# 
+# > Project the financial impact of fare changes.
+
+# In[1]:
+
+
+#hide
+from nbdev.showdoc import *
+
+
+# In[2]:
+
+
+#export
+def say_hello(to):
+    "Say hello to somebody"
+    return f'Hello {to}!'
+
+
+# In[3]:
+
+
+say_hello("Sylvain")
+
+
+# In[4]:
+
+
+assert say_hello("Jeremy")=="Hello Jeremy!"
+
+
+# In[ ]:
+
+
+
+
---
+++ index.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#hide
+from nbdev_demo.core import *
+
+
+# # nbdev_demo
+# 
+# > Just my little demo.
+
+# This file will become your README and also the index of your documentation.
+
+# ## Install
+
+# `pip install nbdev_demo`
+
+# ## How to use
+
+# Fill me in please! Don't forget code examples:
+
+# In[3]:
+
+
+say_hello("Sylvain")
+
+
+# In[ ]:
+
+
+
+
